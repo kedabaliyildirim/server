d347b08e1dccdaa5d418012a671511ff2f17b260
klavas <krst355@gmail.com>
2021-03-18 12:52:01 +0300

    something

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,6 +1,6 @@
 const session = require('express-session')
 // const dataBase = require('../helpers/db.js')
-const reqLogIn = (req, res, next) => {
+const reqLogIn = async (req, res, next) => {
     const something = await req.session.user_id
     if (!something) {
         res.send('error')
