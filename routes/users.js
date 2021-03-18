0ba3ccca3bf93935050a73b8158e0b3604f348d4
klavas <krst355@gmail.com>
2021-03-18 14:28:37 +0300

    something

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const User = require('../models/userSchema.js')
 const bcrypt = require('bcrypt')
 const cors = require('cors');
 const reqLogIn = require('../helpers/auth.js') 
-// const localUrl = 'http://localhost:8080'
+const localUrl = 'http://localhost:8080'
 const url = 'https://vue-test-47cc0.web.app'
 router.use(cors({
   credentials: true,
   origin: {
     url,
-    // localUrl
+    localUrl
   }
 }))
 router.get('/', function (req, res, next) {
