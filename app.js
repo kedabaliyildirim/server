1892f300f1811a4aac8500fabd715e06409cf472
klavas <krst355@gmail.com>
2021-04-02 01:03:04 +0300

    something

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(require('express-session')({
   secret: process.env.SESSION_SECRET,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
-    httpOnly: true,
+    httpOnly: false,
     secure:false
   },
   store: new redisStore({
