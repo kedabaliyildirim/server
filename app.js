e13ba44cff0c3d7d788b2a6166efcf20d773412a
klavas <krst355@gmail.com>
2021-04-02 01:34:13 +0300

    something

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 //ENVIORMENT VERIABLES
 const dotenv = require('dotenv')
@@ -52,6 +52,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //ROUTES
