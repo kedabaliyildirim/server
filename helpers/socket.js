a6eeedc7514b12ce9d54dc5f2a0256c3da49f3db
klavas <krst355@gmail.com>
2021-04-20 02:22:24 +0300

    something

--- helpers/socket.js
+++ helpers/socket.js
@@ -1,14 +1,8 @@
-const app = require('../app')
-const server = require('http').createServer(app);
-const localUrl = 'http://localhost:8080'
-const url = 'https://vue-test-47cc0.web.app'
-const corsUrl = 'https://stormy-mountain-28848.herokuapp.com'
-const io = require('socket.io')(server, {
-   cors: {
-       origin: "http://localhost:8080",
-       methods: ["GET", "POST"]
-   }
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "https://localhost:8080",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["getMeMenager"],
+        credentials: true
+    }
 });
-io.on('connection', () => {});
-server.listen(3002)
-module.exports = io
