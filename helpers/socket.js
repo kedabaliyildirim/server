cfdd4471c65438f99b7a1adaa239b457c3e71a4a
klavas <krst355@gmail.com>
2021-04-20 02:17:42 +0300

    something

--- helpers/socket.js
+++ helpers/socket.js
@@ -4,13 +4,10 @@ const localUrl = 'http://localhost:8080'
 const url = 'https://vue-test-47cc0.web.app'
 const corsUrl = 'https://stormy-mountain-28848.herokuapp.com'
 const io = require('socket.io')(server, {
-    cors: {
-        origin: {
-            localUrl,
-            url,
-            corsUrl
-        }
-    }
+   cors: {
+       origin: "http://localhost:8080",
+       methods: ["GET", "POST"]
+   }
 });
 io.on('connection', () => {});
 server.listen(3002)
