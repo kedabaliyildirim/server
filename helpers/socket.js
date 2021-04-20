c012e4ad010b9c783bfc33d2cd0e49d165a0b11c
klavas <krst355@gmail.com>
2021-04-20 17:26:08 +0300

    something

--- helpers/socket.js
+++ helpers/socket.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const PORT = 3002;
-const server = express().use((req, res) => res.sendFile(INDEX, {
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3003;
+const INDEX = '/index.html';
+
+const server = express()
+    .use((req, res) => res.sendFile({
         root: __dirname
     }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-const socketIO = require('socket.io')(server, {
-    cors: {
-        origin: ["http://localhost:8080",
-            "https://vue-serve-test.herokuapp.com",
-        "https://vue-test-47cc0.web.app"]
-    },
-    Headers: "Access-Control-Allow-Origin"
-});
-const io =socketIO.listen(server)
+
+const io = socketIO(server);
+
 io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-module.exports = io
