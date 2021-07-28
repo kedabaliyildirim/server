e348f722b1789d08ecd9b2a25e2f70b9116331ea
klavas35 <sleepy19@hotmail.com>
2021-07-28 20:56:29 +0300

    something

--- helpers/socket.js
+++ helpers/socket.js
@@ -6,14 +6,7 @@ io.emit('what', 'da')
 io.on('hello', (data) => {
 })
 io.on('connection', (socket) => {
-    socket.on('rockAndRoll', (data) => {
-    })
     socket.on('hello', (data) => {
-    })
-    socket.on('what', (data) => {
-    })
-    socket.emit('hell', 'is')
-    socket.on('hell', (data) => {
     })
 })
 module.exports = socketApi
